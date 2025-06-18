d905cf47b8967c46bd94327886797e64ce97baf6
HaiDank <132982748+HaiDank@users.noreply.github.com>
2025-06-18 12:49:03 +0700

    docker

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,4 +1,24 @@
-import mongoose from "mongoose";
-import 'dotenv/config'
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
-mongoose.connect(process.env.MONGODB_URL ?? '')
+console.log(process.env.MONGODB_URL);
+
+const connectWithRetry = () => {
+	console.log('MongoDB connection attempt...');
+	mongoose
+		.connect(
+			process.env.MONGODB_URL ??
+				'mongodb://mongo:27017/friendmo?replicaSet=rs0'
+		)
+		.then(() => {
+			console.log('db connected');
+		})
+		.catch((err) => {
+			console.error(
+				'❌ MongoDB connection failed, retrying in 5s...',
+				err.message
+			);
+			setTimeout(connectWithRetry, 5000);
+		});
+};
+connectWithRetry();
